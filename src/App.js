7f20e0048e4980cfa6eedef64e743b568418e86b
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-08-05 22:19:12 +0200

    done with components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './css/App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <section className="App">
+      <Calculator />
+    </section>
+  );
+}
+
+export default App;
